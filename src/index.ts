fed9908803a3d3a1ad948e9cca1a778d0a57b6e2
golergka <golergka@gmail.com>
2018-03-20 13:55:45 +0300

    Test /echo command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world")
+import TelegramBot = require("node-telegram-bot-api")
+
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN || "180445993:AAHghLnBrO-e5HgD-1X_J9V1XBQ_qwslpL4";
+console.log("TELEGRAM_TOKEN: " + TELEGRAM_TOKEN)
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    if (match)
+    {
+        const resp = match[1]
+        bot.sendMessage(chatId, resp)
+    }
+});
